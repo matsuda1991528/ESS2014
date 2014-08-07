cefda97a8dff79ff679cd48d81204ec9b7530061
matsuda1991528 <matsuda1991528@yahoo.co.jp>
2014-08-07 16:56:41 +0900

    first commit

---
+++ square.c
@@ -0,0 +1,15 @@
+#include<createoi.h>
+#include<stdio.h>
+
+
+int main(){
+	int a;
+	startOI_MT("/dev/ttyUSB0");
+	while(1){
+		a = runCoverAndDockDemo();
+		printf("a = %d\n", a);
+	}
+	printf("finish\n");
+	stopOI_MT();
+	return 0;
+}
